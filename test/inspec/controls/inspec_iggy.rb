0bbdae4f8431368274e366b4e0aa36ea994ca384
Matt Ray <matthewhray@gmail.com>
2019-12-08 08:46:12 +1100

    more ChefStyle cleanups

--- test/inspec/controls/inspec_iggy.rb
+++ test/inspec/controls/inspec_iggy.rb
@@ -1,12 +1,12 @@
 control "inspec iggy" do
   describe command("bundle exec inspec iggy") do
-    its("stdout") { should match (/inspec iggy help \[COMMAND\]/) }
-    its("stdout") { should match (/inspec iggy version/) }
+    its("stdout") { should match(/inspec iggy help \[COMMAND\]/) }
+    its("stdout") { should match(/inspec iggy version/) }
   end
 end
 
 control "inspec iggy version" do
   describe command("bundle exec inspec iggy version") do
-    its("stdout") { should match (/Iggy v0.7.0/) }
+    its("stdout") { should match(/Iggy v0.7.0/) }
   end
 end
