7ff1923fa581a7f14ab2578cb2e8eac421602821
Matt Ray <matthewhray@gmail.com>
2019-12-16 23:23:28 +1100

    AWS resources for generate/negative testing of two-tier demo
    Full generate/negative coverage for the Terraform Two Tier demo
    https://github.com/terraform-providers/terraform-provider-aws/tree/master/examples/two-tier
    
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- lib/inspec-iggy/platforms/aws_helper.rb
+++ lib/inspec-iggy/platforms/aws_helper.rb
@@ -8,6 +8,7 @@ module InspecPlugins::Iggy::Platforms
     # NOTE: the first entry is going to map to the 'id' from the .tfstate file
     AWS_RESOURCE_QUALIFIERS = {
       "aws_ec2_instance" => %i{instance_id},
+      "aws_elb" => %i{load_balancer_name},
       "aws_security_group" => %i{group_id vpc_id},
       "aws_subnet" => %i{subnet_id},
       "aws_vpc" => %i{vpc_id},
@@ -15,18 +16,26 @@ module InspecPlugins::Iggy::Platforms
 
     # the iterators for the various resource types
     AWS_RESOURCE_ITERATORS = {
+      "aws_ec2_instance" => { "iterator" => "aws_ec2_instances", "index" => "instance_ids", "qualifiers" => [:vpc_id]},
+      "aws_elb" => { "iterator" => "aws_elbs", "index" => "load_balancer_names", "qualifiers" => [:vpc_id]},
+      "aws_security_group" => { "iterator" => "aws_security_groups", "index" => "group_ids", "qualifiers" => [:vpc_id]},
+      "aws_subnet" => { "iterator" => "aws_subnets", "index" => "subnet_ids", "qualifiers" => [:vpc_id]},
+      "aws_vpc" => { "iterator" => "aws_vpcs", "index" => "vpc_ids"},
     }.freeze
 
     AWS_REMOVED_PROPERTIES = {
+      "aws_elb" => %i{health_check security_groups}, # not sure how to test this yet
+      "aws_ec2_instance" => %i{security_groups}, # not sure how to test this yet
     }.freeze
 
     AWS_TRANSLATED_RESOURCE_PROPERTIES = {
+      "aws_elb" => { "name" => "load_balancer_name" },
       "aws_security_group" => { "name" => "group_name" },
     }.freeze
 
     # Terraform boilerplate controls/controls.rb content
     def self.tf_controls
-      "\n\naws_vpc_id = attribute('aws_vpc_id', default: '', description: 'Optional AWS VPC identifier.')\n\n"
+      "\n"
     end
 
     # readme content
